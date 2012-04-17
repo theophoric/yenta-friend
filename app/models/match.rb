3bed943202f2a6f2236cf8af2793902c3f4f9fde
Theodore Wilson <tedawilson@gmail.com>
2012-04-17 15:33:26 -0400

    adding matching

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,22 +2,8 @@ class Match
   include Mongoid::Document
   include Mongoid::Timestamps
   
-  has_and_belongs_to_many :chickstuds
-  embeds_many :chickstud_references
-  
-  after_create :generate_chickstud_references
-  
-  def generate_chickstud_references
-    chickstuds.each{|cs| self.chickstud_references.create(cs.attributes)}
-    save
-  end
-  
-end
-
-class ChickstudReference
-  include Mongoid::Document
-  embedded_in :match
-  
-  field :accepted, :type => Boolean, :default => false
+  belongs_to :owner, :polymorphic => true
+  has_and_belongs_to_many :chickstuds, :inverse_of => nil
+  embeds_many :approvals, :as => :approvable
   
 end
--- app/models/suggestion/approval.rb
+++ app/models/suggestion/approval.rb
@@ -2,7 +2,7 @@ class Approval
   include Mongoid::Document
   
   belongs_to :profile
-  embedded_in :suggestion
+  embedded_in :approvable, :polymorphic => true
   
   field :approved_at, :type => DateTime
   field :approved_message
