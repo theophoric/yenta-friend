7decb7e61b41c1bce9f9ab710b6db0ef8fefcded
Theodore Wilson <tedawilson@gmail.com>
2012-05-09 11:47:02 -0400

    fixes for mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,25 +1,26 @@
 class Notifier < ActionMailer::Base
-	default_url_options[:host] = request.host_with_port
   default :from => "notice@yenta-friend.com"
-  def send_invite(from_profile, email, name = "", message = "")
+  def send_invite(from_profile, email, host = "http://beta.yenta-friend.com", name = "", message = "")
     @from_profile = from_profile
     @name = name
     @message = message
     mail(
       :to => email,
       :bcc => "yentafriend@gmail.com",
-      :subject => "Yenta-Friend Beta Invite"
+      :subject => "Yenta-Friend Beta Invite",
+			:host => host
     )
   end
 
-	def send_connection(matched_profile, email, connection, name = "")
+	def send_connection(matched_profile, email, connection, host = "http://beta.yenta-friend.com", name = "")
 		@matched_profile = matched_profile
 		@connection = connection
 		@name = name
     mail(
       :to => email,
       :bcc => "yentafriend@gmail.com",
-      :subject => "You have a new connection on YentaFriend"
+      :subject => "You have a new connection on YentaFriend",
+			:host => host
     )
 	end
 end
